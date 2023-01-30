52f693b7268e28d14e61deab1cc1f248f7c1a9d6
Elena Solomko <lena06051981be@gmail.com>
2023-01-30 02:56:49 +0200

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,37 @@
-// import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Main } from "./Title.styled";
-import Stack from '@mui/material/Stack';
+import { ContentText, ContentTitle, HeroSection, Img, ImgWrapper, Main } from "./Title.styled";
+import { Container, Paper, Typography } from "@mui/material";
+import image from '../images/writing-923882.jpg';
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
 
 export const Home = () => {
     return (
       <Main>
-        <h1>Welcome to your phonebook application!</h1>
-        <Stack
-          direction="row"
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          sx={{ maxWidth: '600px', margin: '0 auto' }}
-        >
-          {/* <Image name="home.jpg" alt="A man looks at his laptop " w="300" /> */}
-          <p>
-            This app allows you to be always close to your contacts. To start
-            using it right now, just <b><Link to="/register">create</Link></b> an account, it will not take much time, but you will be sure that you
-            already have access to them!
-          </p>
-        </Stack>
+        <Container sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: 4,
+          pb: 4
+        }} maxWidth="xl">
+          <AutoStoriesIcon sx={{ fontSize: 80, mb: 2, color: '#0f7ec9' }} />        
+          <Typography variant="h1" sx={{ fontStyle: 'italic', fontFamily: 'monospace', fontSize: '24px', fontWeight: '700',}}>Welcome to your  <span style={{ color: '#0f7ec9', }} >Phonebook</span> application!</Typography>
+          <HeroSection>
+            <ImgWrapper>
+              <Img src={image} alt="woman width notebook" width="320" />
+            </ImgWrapper>
+            <Paper sx={{ p: 4, fontFamily: 'monospace',  }}   elevation={3}>
+                <ContentTitle>About</ContentTitle>
+                <ContentText>
+                  Phonebook app is an easy to use contact manager app that gives you facility 
+                  of saving and viewing your contacts, so you never lose your contacts.
+                  To use the application right now, just <b><Link to="/register">create</Link></b> an account, 
+                  and your contacts will always be close to you at any time!
+                </ContentText>
+            </Paper>
+          </HeroSection>
+        </Container>
       </Main>
     );
   };
--- src/pages/Title.styled.jsx
+++ src/pages/Title.styled.jsx
@@ -36,3 +36,71 @@ export const Main = styled.main`
   padding-top: 54px;  
 `
 /* background-image: url('${image }'); */
+
+export const HeroSection = styled.section`
+  margin-top: 60px;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+  }
+  @media screen and (min-width: 768px) {
+    gap: 50px;
+  }
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 500px;
+  object-fit: cover;
+  object-position: 87% 50%;
+  border-radius: 20px;
+  @media screen and (max-width: 920px) {
+    height: 400px;
+  }
+  @media screen and (max-width: 479px) {
+    height: 300px;
+  }
+`;
+
+export const ContentTitle = styled.h2`
+  font-size: 24px;
+  line-height: 1.4;
+  position: relative;
+  margin-bottom: 20px;
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    display: block;
+    height: 1px;
+    width: 300px;
+    background-color: #000;
+    @media screen and (max-width: 830px) {
+      width: 200px;
+      font-size: 20px;
+    }
+  }
+`;
+
+export const ImgWrapper = styled.div`
+  width: calc(100% / 2);
+  flex-shrink: 0;
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+`;
+
+export const ContentText = styled.p`
+  font-size: 24px;
+  line-height: 1.7;
+  @media screen and (max-width: 1120px) {
+    font-size: 20px;
+  }
+  @media screen and (max-width: 920px) {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+`;
